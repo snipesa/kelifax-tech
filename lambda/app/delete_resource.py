b499d0347c0a484bfb1f0750c03ad71b121f431c
Ashime Etunyi <ashimeetunyi@Mac.lan>
2025-10-20 20:05:55 -0400

    updates

--- lambda/app/delete_resource.py
+++ lambda/app/delete_resource.py
@@ -1,6 +1,6 @@
 import json
 
-def handle_delete_resource(event, headers):
+def handle_delete_resource(event, headers, table_name):
     """Handle resource deletion"""
     path_params = event.get('pathParameters') or {}
     resource_slug = path_params.get('slug') or event.get('path', '').split('/')[-1]
--- lambda/app/update_resource.py
+++ lambda/app/update_resource.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 
-def handle_update_resource(event, headers):
+def handle_update_resource(event, headers, table_name):
     """Handle resource approval/rejection"""
     path_params = event.get('pathParameters') or {}
     resource_slug = path_params.get('slug') or event.get('path', '').split('/')[-1]
